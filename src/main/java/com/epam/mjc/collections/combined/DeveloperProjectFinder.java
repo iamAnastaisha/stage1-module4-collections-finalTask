f92058f855c0bc3098b657d1c72a66bc34d5a87a
Nastya Garkavaya <anastaishastil2002@gmail.com>
2022-10-30 20:02:37 +0300

    Updated

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,24 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> resultList = new ArrayList<>();
+        for (String key : projects.keySet()) {
+            if (projects.get(key).contains(developer)) {
+                resultList.add(key);
+            }
+        }
+        resultList.sort((o1, o2) -> {
+            if (o1.length() < o2.length()) {
+                return 1;
+            } else if (o1.length() > o2.length()) {
+                return -1;
+            } else {
+                return -o1.compareTo(o2);
+            }
+        });
+        return resultList;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+        for (String key : sourceMap.keySet()) {
+            if (resultMap.containsKey(key.length())) {
+                Set<String> helper = resultMap.get(key.length());
+                helper.add(key);
+                resultMap.put(key.length(), helper);
+            } else {
+                Set<String> set = new HashSet<>();
+                set.add(key);
+                resultMap.put(key.length(), set);
+            }
+        }
+        return resultMap;
     }
 }
